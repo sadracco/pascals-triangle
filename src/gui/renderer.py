7961e165769f26caa066fe8c18600aff9e531638
sadracco <krempcek@gmail.com>
2019-12-14 16:05:43 +0100

    Renderer

--- src/gui/renderer.py
+++ src/gui/renderer.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QPainter, QBrush, QColor
+from PyQt5.QtGui import QPainter, QBrush, QColor, QPolygonF
 from calc_utils import pascal, parse_layer_def
 
 
@@ -7,20 +7,21 @@ class Renderer(QWidget):
     def __init__(self):
         super(Renderer, self).__init__()
         self.qp = QPainter()
+        self.size = 30
 
     def paintEvent(self, event):
         self.qp.begin(self)
         self.render_triangle(pascal(parse_layer_def(';6,r;')))
         self.qp.end()
 
-    def draw_cell_rect(x,y,color):
-        pass
+    def draw_cell_rect(self, x, y, color):
+        self.qp.setBrush(QBrush(color))
+        self.qp.drawRect(x*self.size + y*(self.size/2), y*self.size, self.size, self.size)
 
-    def draw_cell_hex(x,y,color):
+    def draw_cell_hex(x,y,s,color):
         pass
 
     def render_triangle(self, triangle):
         for y, layer in enumerate(triangle):
             for x, cell in enumerate(layer):
-                self.qp.setBrush(QBrush(QColor(120 * cell, 0,0)))
-                self.qp.drawRect(20*x + 10*y, y*20, 20, 20)
+                self.draw_cell_rect(x,y,QColor(255,0,0))
